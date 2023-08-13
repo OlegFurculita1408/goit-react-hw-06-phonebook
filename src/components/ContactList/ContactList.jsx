4291b37d8f221bcc870036495daf268b71d4d23e
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-08-13 19:26:01 +0300

    required filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,32 +1,32 @@
 import { nanoid } from "nanoid";
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from '../redux/contactSlice';
-import { selectContacts } from "components/redux/selectors";
+import { selectVisibleContact } from "components/redux/selectors";
 import css from './ContactList.module.css';
 import { toast } from "react-toastify";
 
 
 const ContactList = () => {
     const dispatch = useDispatch();
-    const contacts = useSelector(selectContacts);
+    const contacts = useSelector(selectVisibleContact);
     const notify = () => toast.warning(`Delete contact!`, {position: toast.POSITION.TOP_LEFT});
     
         return (
             <>
-            <ul className={css.contactList}>
-                {contacts.map(item => {
-                    return (
-                        <li key={nanoid()} className={css.contactsItem}>
-                        <p>
-                            {item.name}: <span>{item.number}</span>
-                        </p>
-                        <button className={css.itemBtn}
-                                onClick={() => dispatch(deleteContact(item.id),
-                                                 notify())}>Delete</button>
-                        </li>
-                    );
-                })}
-            </ul>
+                <ul className={css.contactList}>
+                    {contacts.map(item => {
+                        return (
+                            <li key={nanoid()} className={css.contactsItem}>
+                            <p>
+                                {item.name}: <span>{item.number}</span>
+                            </p>
+                            <button className={css.itemBtn}
+                                    onClick={() => dispatch(deleteContact(item.id),
+                                                    notify())}>Delete</button>
+                            </li>
+                        );
+                    })}
+                </ul>
             </>
         );
     }
--- src/components/Filters/Filter.jsx
+++ src/components/Filters/Filter.jsx
@@ -2,21 +2,26 @@ import { useDispatch, useSelector } from 'react-redux';
 import { filterContacts } from '../redux/filterSlice';
 import css from './Filter.module.css';
 
-const Filter = () => {
-    const dispatch = useDispatch();
-    const filter = useSelector(state => state.filter);
 
-    const handleFilterChange = e => {
-        dispatch(filterContacts(e.target.value));
+const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.filter);
+    
+  const handleFilterChange = e => {
+    dispatch(filterContacts(e.target.value));
   };
-        return (
-            <div className={css.containerFilter}>
-                <label htmlFor="fiind">Find contacts by name</label>
-                    <input type="text"
-                        onChange={handleFilterChange}
-                        value={filter}
-                />
-            </div>
-            )
-}
+
+  return (
+    <div className={css.containerFilter}>
+        <label>
+            <input
+                type="text"
+                placeholder="Find contacts by name"
+                value={filter}
+                onChange={handleFilterChange}
+            />
+        </label>
+    </div>
+  );
+};
 export default Filter
